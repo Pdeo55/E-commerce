e5192622af8c4ed4edc88e3019a6bde5bc4a15c4
Pdeo55 <88934417+Pdeo55@users.noreply.github.com>
2022-07-11 22:07:31 +0530

    trying url

--- myweb1/settings.py
+++ myweb1/settings.py
@@ -77,15 +77,14 @@ WSGI_APPLICATION = 'myweb1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'd2qtdhnavg9tbs',
-        'HOST': 'ec2-52-205-61-230.compute-1.amazonaws.com',
-        'PORT' : 5432,
-        'USER' :'abedzadervsxcl',
-        'PASSWORD' :'769621f3cfef26518624457202e0c650dbca48db5d88355d84c3e95eb70e20f9'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
